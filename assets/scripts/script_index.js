6900a501ebc9745e8ebcdb7b96d179b47512458d
Christopher-VA <cvonaltenstadt@gmail.com>
2023-11-21 21:32:08 +0800

    added local storage for searched input

--- assets/scripts/script_index.js
+++ assets/scripts/script_index.js
@@ -1,7 +1,12 @@
 var searchInput = document.getElementById("search-input");
+var lastSearched = localStorage.getItem(searchInput);
 
 document.querySelector('form.search-form').addEventListener('submit', function (e) {
     e.preventDefault();
 
+    var text = searchInput.value;
+    localStorage.setItem(searchInput, text);
     console.log(searchInput.value);
 })
+
+console.log(lastSearched);
